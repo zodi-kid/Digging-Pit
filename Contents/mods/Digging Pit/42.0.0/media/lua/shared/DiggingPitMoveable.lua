e3aebe318d1c7f896624616de75004c4c72ea4f2
zodi-kid <monstruono@gmail.com>
2026-08-28 12:18:58 +0200

    Progress on custom scrapping
    Small changes.
    
    Currently, it detects the tile as scrappable, and walks to it when clicked, but fails to perform the action

--- Contents/mods/Digging Pit/42.0.0/media/lua/shared/DiggingPitMoveable.lua
+++ Contents/mods/Digging Pit/42.0.0/media/lua/shared/DiggingPitMoveable.lua
@@ -1,14 +1,17 @@
-local function diggingPitDefinition()
+-- Create new scrapping definition with new 'material' associated
+local function setDiggingPitDefinition()
+	-- ISMoveableDefinitions is supposed to be a singleton, so using it this way should be fine
     local moveableDefinitions = ISMoveableDefinitions.getInstance()
     if not moveableDefinitions then
         print("[DiggingPit]: 	ERROR: ISMoveableDefinitions.getInstance() returned nil")
         return
     end
-    moveableDefinitions.addScrapDefinition( "DiggingPit", {"Base.Shovel"}, {}, Perks.Woodwork, 0, "Shoveling", true);
-    moveableDefinitions.addScrapItem("DiggingPit", "Base.Stone2", 4, 90);    
-    print("[DiggingPit]: 	Added Digging Pit to Moveable Definitions")
+	-- Use material 'DiggingPit', which we add in the next line; Perks.Woodwork is a placeholder to avoid errors
+    moveableDefinitions.addScrapDefinition( "DiggingPit", {"Base.Shovel"}, {}, Perks.Woodwork, 10, "Shoveling", true);
+    moveableDefinitions.addScrapItem("DiggingPit", "Base.Worm", 4, 90);
 end
 
+-- Because TileZed only allows materials from vanilla, we have to change it dynamically
 local function setDiggingPitMaterial()
     local spriteNames = {
         "digging_pit_0",
@@ -16,20 +19,18 @@ local function setDiggingPitMaterial()
         "digging_pit_2",
         "digging_pit_3",
     }
-
+	-- Change the 'material' property for all sprites
     for _, spriteName in ipairs(spriteNames) do
         local sprite = getSprite(spriteName) -- from IsoSpriteManager; returns IsoSprite
-
         if sprite then
-            local properties = sprite:getProperties()
+			local properties = sprite:getProperties()
             properties:set("Material", "DiggingPit")
-
-            print("[DiggingPit] Set Material=DiggingPit for " .. spriteName)
+			--properties:set("Material", "Natural")
         else
-            print("[DiggingPit] ERROR: Could not find sprite " .. spriteName)
+            print("[DiggingPit]:	ERROR: Could not find sprite " .. spriteName)
         end
     end
 end
 
-Events.OnGameBoot.Add(diggingPitDefinition)
-Events.OnGameStart.Add(setDiggingPitMaterial)
+Events.OnGameBoot.Add(setDiggingPitDefinition)
+Events.OnInitGlobalModData.Add(setDiggingPitMaterial)
