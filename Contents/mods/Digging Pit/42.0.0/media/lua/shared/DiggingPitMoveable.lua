b07847658be6c34d112aa363b4ce5bd9eecfbc35
zodi-kid <monstruono@gmail.com>
2026-08-31 16:44:43 +0200

    Disassembly implemented
    Added the DiggingPit material by editing the file TileProperties.txt, at C:/Users/usr/.TileZed/ adding my custom material, and setting the property through TileZed.

--- Contents/mods/Digging Pit/42.0.0/media/lua/shared/DiggingPitMoveable.lua
+++ Contents/mods/Digging Pit/42.0.0/media/lua/shared/DiggingPitMoveable.lua
@@ -7,30 +7,9 @@ local function setDiggingPitDefinition()
         return
     end
 	-- Use material 'DiggingPit', which we add in the next line; Perks.Woodwork is a placeholder to avoid errors
-    moveableDefinitions.addScrapDefinition( "DiggingPit", {"Base.Shovel"}, {}, Perks.Woodwork, 10, "Shoveling", true);
+	--moveableDefinitions.addToolDefinition("DiggingPit", {"Base.Shovel"}, Perks.Woodwork, 10, "Shoveling", true);
+    moveableDefinitions.addScrapDefinition("DiggingPit", {"Base.Shovel"}, {}, Perks.Woodwork, 10, "Shoveling", true);
     moveableDefinitions.addScrapItem("DiggingPit", "Base.Worm", 4, 90);
 end
 
--- Because TileZed only allows materials from vanilla, we have to change it dynamically
-local function setDiggingPitMaterial()
-    local spriteNames = {
-        "digging_pit_0",
-        "digging_pit_1",
-        "digging_pit_2",
-        "digging_pit_3",
-    }
-	-- Change the 'material' property for all sprites
-    for _, spriteName in ipairs(spriteNames) do
-        local sprite = getSprite(spriteName) -- from IsoSpriteManager; returns IsoSprite
-        if sprite then
-			local properties = sprite:getProperties()
-            properties:set("Material", "DiggingPit")
-			--properties:set("Material", "Natural")
-        else
-            print("[DiggingPit]:	ERROR: Could not find sprite " .. spriteName)
-        end
-    end
-end
-
 Events.OnGameBoot.Add(setDiggingPitDefinition)
-Events.OnInitGlobalModData.Add(setDiggingPitMaterial)
